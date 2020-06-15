9f17d6460c958ca60de7a1acb12581126b212981
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-06-15 13:47:06 -0700

    deserialize into type

--- src/bin/validator-mgmt.rs
+++ src/bin/validator-mgmt.rs
@@ -1,40 +1,37 @@
 mod yaml_config {
+    use std::collections::BTreeMap;
+
     use serde::{Deserialize, Serialize};
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     pub struct BLSKeyManagement {
-        adjust_keys_off_median_every: i64,
-        shard_0_keys: Vec<String>,
-    }
-
-    #[derive(Debug, PartialEq, Serialize, Deserialize)]
-    pub struct EmailSender {
-        account_name: String,
-        account_password: String,
+        pub adjust_keys_off_median_every: i64,
+        pub shard_0_keys: Vec<String>,
     }
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     pub struct Notifications {
-        enabled: bool,
-        email_sender: EmailSender,
+        pub enable: bool,
+        // TODO these could be named types
+        pub email_sender: BTreeMap<String, String>,
+        pub email_receiver: BTreeMap<String, String>,
+        pub mobile_phone: BTreeMap<String, String>,
     }
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     pub struct Manage {
-        mainnet_account_addr: String,
-        collect_rewards_every: i64,
-        rpc_endpoint: String,
-        bls_key_management: BLSKeyManagement,
-        notifications: Notifications,
+        pub mainnet_account_addr: String,
+        pub collect_rewards_every: i64,
+        pub rpc_endpoint: String,
+        pub bls_key_management: BLSKeyManagement,
+        pub notifications: Notifications,
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let f = std::fs::File::open("example.yaml")?;
-    // let d: String = serde_yaml::from_reader(f)?;
+    let f = std::fs::File::open("example.yaml")?;
+    let m: yaml_config::Manage = serde_yaml::from_reader(f)?;
 
-    // let input_config = std::fs::read_to_string("example.yaml")?;
-
-    // println!("Read YAML string: {:?}", f);
+    println!("Read YAML string: {:?}", m);
     Ok(())
 }
